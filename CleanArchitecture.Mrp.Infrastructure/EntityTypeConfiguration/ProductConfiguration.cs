2d7858a6eb0a1761e2f649b0e6479f38e1890b6b
Muhammet Esat Demir <muhammetesatdemir@outlook.com>
2025-12-30 20:07:24 +0300

    product ve user entity configurationların eklenmesi

---
+++ CleanArchitecture.Mrp.Infrastructure/EntityTypeConfiguration/ProductConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Mrp.Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Mrp.Infrastructure.EntityTypeConfiguration
+{
+    public class ProductConfiguration : BaseEntityConfiguration<Product>
+    {
+        public override void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable(name: "Products");
+            builder.Property(e => e.Name).HasColumnName("Name").HasMaxLength(100).IsRequired();
+            builder.Property(e => e.Code).HasColumnName("Code").HasMaxLength(100).IsRequired();
+            builder.Property(e => e.StockQuantity).HasDefaultValue(0);
+            base.Configure(builder);
+        }
+    }
+}
---
+++ CleanArchitecture.Mrp.Infrastructure/EntityTypeConfiguration/UserConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Mrp.Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Mrp.Infrastructure.EntityTypeConfiguration
+{
+    public class UserConfiguration : BaseEntityConfiguration<User>
+    {
+        public override void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable(name: "Users");
+            builder.Property(e => e.UserName).HasColumnName("UserName").HasMaxLength(100).IsRequired();
+            builder.Property(e => e.PasswordHash).HasColumnName("PasswordHash").IsRequired();
+            base.Configure(builder);
+        }
+    }
+}
